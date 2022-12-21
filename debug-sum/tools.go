62edcfc70aefd0cb65f397eac9727c29f8c9940e
Viktor <v.kostov@sap.com>
2022-12-21 16:03:21 +0200

    Create tools.go

---
+++ debug-sum/tools.go
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build tools
+// +build tools
+
+// This package imports things required by build scripts, to force `go mod` to see them as dependencies
+package tools
+
+import (
+	_ "github.com/ahmetb/gen-crd-api-reference-docs"
+)
